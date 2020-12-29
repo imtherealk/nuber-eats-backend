1f2e5f3d615287b1910633051ef7d81672118cd0
Nayeong <imtherealk@gmail.com>
2020-12-29 20:45:39 +0900

    Send verification email if an account is created

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,45 +2,49 @@ import { Inject, Injectable } from '@nestjs/common';
 import * as FormData from 'form-data';
 import got from 'got';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
-import { MailModuleOptions } from './mail.interaces';
+import { EmailVar, MailModuleOptions } from './mail.interaces';
 
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    this.sendEmail(
-      '[Nuber-Eats] Confirm Your Account',
-      'verify-email',
-      'imtherealk@gmail.com',
-    );
-  }
+  ) {}
 
   private async sendEmail(
     subject: string,
     template: string,
     userEmail: string,
+    emailVars: EmailVar[],
   ) {
     const form = new FormData();
-    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('from', `Nuber Eats <mailgun@${this.options.domain}>`);
     form.append('to', userEmail);
     form.append('subject', subject);
     form.append('template', template);
-    form.append('v:username', 'margot');
-    form.append('v:code', 'testtest');
+    emailVars.forEach(eVar => form.append(`v:${eVar.key}`, eVar.value));
 
-    const response = await got(
-      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `api:${this.options.apiKey}`,
-          ).toString('base64')}`,
+    try {
+      const response = await got(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
         },
-        body: form,
-      },
-    );
-    console.log(response.body);
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  sendVerificationEmail(email: string, code: string) {
+    this.sendEmail('Confirm Your Account', 'verify-email', email, [
+      { key: 'code', value: code },
+      { key: 'username', value: email },
+    ]);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
 import { Repository } from 'typeorm';
 import {
   CreateAccountInput,
@@ -20,6 +21,7 @@ export class UsersService {
     @InjectRepository(Verification)
     private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
+    private readonly mailService: MailService,
   ) {}
 
   async createAccount({
@@ -38,7 +40,10 @@ export class UsersService {
       const user = await this.users.save(
         this.users.create({ email, password, role }),
       );
-      await this.verifications.save(this.verifications.create({ user }));
+      const verification = await this.verifications.save(
+        this.verifications.create({ user }),
+      );
+      this.mailService.sendVerificationEmail(user.email, verification.code);
       return { success: true };
     } catch (error) {
       return { success: false, error: "Couldn't create account" };
@@ -87,7 +92,10 @@ export class UsersService {
         user.email = email;
         user.verified = false;
 
-        await this.verifications.save(this.verifications.create({ user }));
+        const verification = await this.verifications.save(
+          this.verifications.create({ user }),
+        );
+        this.mailService.sendVerificationEmail(user.email, verification.code);
       }
       if (password) {
         user.password = password;
