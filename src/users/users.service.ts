5da767da4f18c6e27579dcae75d9480f4d6810dc
Nayeong <imtherealk@gmail.com>
2020-12-29 17:42:55 +0900

    Delete verification when email is verified

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -108,6 +108,7 @@ export class UsersService {
       if (verification) {
         verification.user.verified = true;
         await this.users.save(verification.user);
+        await this.verifications.delete(verification.id);
         return {
           success: true,
         };
