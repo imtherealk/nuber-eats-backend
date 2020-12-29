9f942394ca3b1fa80cd434c1f1b8157c49e1f903
Nayeong <imtherealk@gmail.com>
2020-12-29 20:16:54 +0900

    Apply mailgun template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -9,15 +9,25 @@ export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
-    // this.sendEmail('Hello', 'It works!', 'imtherealk@gmail.com');
+    this.sendEmail(
+      '[Nuber-Eats] Confirm Your Account',
+      'verify-email',
+      'imtherealk@gmail.com',
+    );
   }
 
-  private async sendEmail(subject: string, content: string, userEmail: string) {
+  private async sendEmail(
+    subject: string,
+    template: string,
+    userEmail: string,
+  ) {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', userEmail);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', template);
+    form.append('v:username', 'margot');
+    form.append('v:code', 'testtest');
 
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
